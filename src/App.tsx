7a05a25e96eb6d4ad43d3e2e48d1741bcb9136bf
ozdemirilayda <ozdemirilayda95@gmail.com>
2024-04-18 20:25:32 +0300

    Add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
+import { Container } from "react-bootstrap";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Store from "./pages/Store";
+import About from "./pages/About";
+
 function App() {
   return (
-    <>
-      <h1>hello</h1>
-      <h2>Salut</h2>
-      <h3>Salve</h3>
-      <h4>Merhaba</h4>
-    </>
+    <Container>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/store" element={<Store />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Container>
   );
 }
 
