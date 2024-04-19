279822b1ab4700eb5e5568e950ac5eda33ffd96d
ozdemirilayda <ozdemirilayda95@gmail.com>
2024-04-19 19:49:40 +0300

    Update store file by data mapping

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,3 +1,14 @@
+import { Col, Row } from "react-bootstrap";
+import storeItems from "../data/items.json";
 export default function Store() {
-  return <h1>Store</h1>;
+  return (
+    <>
+      <h1>Store</h1>
+      <Row>
+        {storeItems.map((item, key) => (
+          <Col key={key}>{JSON.stringify(item)}</Col>
+        ))}
+      </Row>
+    </>
+  );
 }
